d825ca4fbb2ce0da30d8b37789a581a565f89097
Klein Florian <florian.klein@free.fr>
2019-03-08 09:54:16 +0100

    use ext-async instead
    Signed-off-by: Klein Florian <florian.klein@free.fr>

--- example/async.php
+++ example/async.php
@@ -1,42 +1,48 @@
 <?php declare(strict_types=1);
 
 require(__DIR__.'/../vendor/autoload.php');
+require(__DIR__.'/functions.php');
 
 use Amp\Loop;
 use Amp\Socket\ClientTlsContext;
 use Amp\Artax\DefaultClient;
 use Amp\Artax\Request;
 use Amp\Artax\StringBody;
+use function amp\fiber\{await,coroutine};
 use Hippiemedia\Agent\Client;
 use Hippiemedia\Agent\Client\Response;
-use function Amp\GreenThread\{await,coroutine};
 
-$api = require(__DIR__.'/api.php');
-$ampClient = new DefaultClient(null, null, (new ClientTlsContext)->withoutPeerVerification());
+Loop::run(coroutine(function() use($argv) {
+    $api = require(__DIR__.'/api.php');
+    $ampClient = new DefaultClient(null, null, (new ClientTlsContext)->withoutPeerVerification());
 
-Loop::run(coroutine(function() use($api, $ampClient) {
-    $api(new class($ampClient) implements Client { function __invoke($method, $uri, array $params = [], $headers = []): Response {
-        $response = await($this->ampClient->request((new Request($uri, $method))
-            ->withHeaders($headers)
-            ->withBody(new StringBody(http_build_query($params)))
-        ));
+    $api(new class($ampClient) implements Client {
+        public function __construct($ampClient) { $this->ampClient = $ampClient; }
 
-        return new class($response) implements Response {
-            private $response;
-            public function __construct($response)
-            {
-                $this->response = $response;
-            }
+        public function __invoke($method, $uri, array $params = [], $headers = []): Response
+        {
+            $response = await($this->ampClient->request((new Request($uri, $method))
+                ->withHeaders($headers)
+                ->withBody(new StringBody(http_build_query($params)))
+            ));
 
-            public function getHeader(string $header): ?string
-            {
-                return $this->response->getHeader($header);
-            }
+            return new class($response) implements Response {
+                private $response;
+                public function __construct($response)
+                {
+                    $this->response = $response;
+                }
 
-            public function getBody(): string
-            {
-                return await($this->response->getBody());
-            }
-        };
-    } function __construct($ampClient) { $this->ampClient = $ampClient; }});
+                public function getHeader(string $header): ?string
+                {
+                    return $this->response->getHeader($header);
+                }
+
+                public function getBody(): string
+                {
+                    return await($this->response->getBody());
+                }
+            };
+        }
+    });
 }));
--- example/sync.php
+++ example/sync.php
@@ -7,41 +7,45 @@ require(__DIR__.'/../vendor/autoload.php');
 
 $api = require(__DIR__.'/api.php');
 
-$api(new class implements Client { function __invoke($method, $uri, array $params = [], array $headers = []): Response {
-    $body = file_get_contents($uri, false, stream_context_create($options = [
-        'http' => [
-            'method' => $method,
-            'header' => array_map(function($key, $value) {
-                return "$key: $value";
-            }, array_keys($headers), $headers),
-            'content' => http_build_query($params),
-        ],
-        'ssl' => ['allow_self_signed' => true, 'verify_peer' => false, 'verify_peer_name' => false],
-    ]));
-    getenv('DEBUG') === '1' && var_dump($options, $body);
+$api(new class implements Client {
+    public function __invoke($method, $uri, array $params = [], array $headers = []): Response
+    {
+        $body = file_get_contents($uri, false, stream_context_create($options = [
+            'http' => [
+                'method' => $method,
+                'header' => array_map(function($key, $value) {
+                    return "$key: $value";
+                }, array_keys($headers), $headers),
+                'content' => http_build_query($params),
+            ],
+            'ssl' => ['allow_self_signed' => true, 'verify_peer' => false, 'verify_peer_name' => false],
+        ]));
+        getenv('DEBUG') === '1' && var_dump($options, $body);
 
-    return new class($body, $http_response_header) implements Response {
-        public function __construct($body, array $headers)
+        return new class($body, $http_response_header) implements Response
         {
-            $this->body = $body;
-            array_shift($headers);
-            $this->headers = array_reduce($headers, function($carry, $item) {
-                if (!strpos($item, ':')) return ;
-                [$key, $value] = explode(': ', $item);
-                $carry[$key] = $value;
-                return $carry;
-            }, []);
-        }
+            public function __construct($body, array $headers)
+            {
+                $this->body = $body;
+                array_shift($headers);
+                $this->headers = array_reduce($headers, function($carry, $item) {
+                    if (!strpos($item, ':')) return ;
+                    [$key, $value] = explode(': ', $item);
+                    $carry[$key] = $value;
+                    return $carry;
+                }, []);
+            }
 
-        public function getHeader(string $header): ?string
-        {
-            return $this->headers[$header] ?? '';
-        }
+            public function getHeader(string $header): ?string
+            {
+                return $this->headers[$header] ?? '';
+            }
 
-        public function getBody(): string
-        {
-            return $this->body;
-        }
-    };
-}});
+            public function getBody(): string
+            {
+                return $this->body;
+            }
+        };
+    }
+});
 
